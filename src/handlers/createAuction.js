5c819322e7586a60016069a4c51104eb4fc99776
saideepesh000 <saideepesh000@gmail.com>
2020-10-08 19:36:06 +0530

    added: Dynamodb

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,12 +1,23 @@
+import {v4 as uuid} from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.Dynamodb.DocumentClient();
+
 async function createAuction(event, context) {
   const {title} = JSON.parse(event.body);
   const createdTime = new Date();
 
   const auction = {
+	id: uuid(),
 	title,
 	status: 'OPEN',
 	createdAt: createdTime.toISOString(),
   };
+
+  await dynamodb.put({
+	TableName: 'AuctionsTable',
+	Item: auction
+  }).promise();
 
   return {
     statusCode: 201,
