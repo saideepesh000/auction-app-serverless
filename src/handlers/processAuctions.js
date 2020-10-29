b49b03b2f7ded54dd7e4c316996fe5b000f75812
saideepesh000 <saideepesh000@gmail.com>
2020-10-29 23:32:09 +0530

    closeAuctions

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,8 +1,17 @@
+import createError from 'http-errors';
 import {getEndedAuctions} from '../helper/getEndedAuctions';
+import {closeAuction} from '../helper/closeAuction.js';
 
 async function processAuctions(event, context){
+	try{
 	const auctionsToClose = await getEndedAuctions();
-	console.log(auctionsToClose);
+	const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+	await Promise.all(closePromises);
+	return {closed: closePromises.length};
+	} catch(err){
+		console.error(err);
+		throw new createError.InternalServerError(err);
+	}
 }
 
 export const handler = processAuctions;
